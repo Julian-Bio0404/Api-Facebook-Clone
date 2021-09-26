2230a8ba155d6699c76ccacaa0fa3c1c8bb783a1
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2021-09-25 19:53:42 -0500

    Consumers.

---
+++ facebook/app/chats/consumers.py
@@ -0,0 +1,21 @@
+"""Chats consumers."""
+
+# Channels
+from channels.consumer import SyncConsumer
+
+
+class EchoConsumer(SyncConsumer):
+    """Consumer."""
+
+    def websocket_connect(self, event):
+        print('Connect event is called.')
+        self.send({'type': 'websocket.accept'})
+
+    def websocket_receive(self, event):
+        print(event)
+        self.send(
+            {'type': 'websocket.send', 'text': event.get('text')})
+
+    def websocket_disconnect(self, event):
+        print('Connection is disconected.')
+        print(event)
--- facebook/facebook_settings/routing.py
+++ facebook/facebook_settings/routing.py
@@ -1,7 +1,17 @@
 """Routing configuration."""
 
+# Django
+from django.urls import path
+
 # Channels
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+# Consumers
+from app.chats.consumers import EchoConsumer
 
 
-application = ProtocolTypeRouter({})
+application = ProtocolTypeRouter({
+    'websocket': URLRouter([
+        path('ws/chats/', EchoConsumer)
+    ])
+})
